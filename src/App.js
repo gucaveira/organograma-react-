ec9efdcda9cb6b1684180a127c3382bc3f7ea694
gustavo pereira <pereiragustavo215@gmail.com>
2023-07-06 17:06:12 -0300

    criando componente CampoTexto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Banner from './componentes/Banner/Banner';
+import CampoTexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
+      <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
+      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
     </div>
   );
 }
