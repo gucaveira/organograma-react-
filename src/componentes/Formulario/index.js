7773ba25717e78a757d8fb4a4b4566f11029ac28
gustavo pereira <pereiragustavo215@gmail.com>
2023-07-06 17:44:56 -0300

    criando componente lista suspensa

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 
 const Formulario = () => {
+
+    const times = [
+        'programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return (
         <section className='formulario'>
             <form >
@@ -9,6 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu Nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Time" itens={times} />
             </form>
         </section>
     )
