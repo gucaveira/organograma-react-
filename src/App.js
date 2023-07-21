c8b1dc870ad5fd16f8444632e1f7a2428619df4a
gustavo pereira <pereiragustavo215@gmail.com>
2023-07-21 20:09:10 -0300

    enviando o objeto de colaborador para tela com lista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 
 function App() {
+
+  const [colaboradores, setcolaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+      console.log(colaborador)
+      setcolaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={ 
+        colaborador => aoNovoColaboradorAdicionado(colaborador) }/>
     </div>
   );
 }
