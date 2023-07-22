a04792c9147c3f75d72cfa7ab1210396fb5bc5d0
gustavo pereira <pereiragustavo215@gmail.com>
2023-07-21 21:02:26 -0300

    definindo css para o componente time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -16,6 +17,9 @@ function App() {
       <Banner />
       <Formulario aoColaboradorCadastrado={ 
         colaborador => aoNovoColaboradorAdicionado(colaborador) }/>
+        <Time nome="Programação"/>
+        <Time nome="Frontend"/>
+        <Time nome="Data Science"/>
     </div>
   );
 }
